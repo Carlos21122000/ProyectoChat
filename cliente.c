70d8ce079f0618c401a547801d2444ec8c61f4ac
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2022-04-26 22:52:26 -0600

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,76 +1,97 @@
-// Plantilla de un Menú de Opciones
-
-#include <iostream>
-#include <cstdlib>
-
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <json/json.h>
 
 int main()
 {
-    int opcion;
-    bool repetir = true;
-    
-    do {
-        system("cls");
-        
-        // Texto del menú que se verá cada vez
-        cout << "\n\nMenu de Opciones" << endl;
-        cout << "1. Chat con todods lo usuarios" << endl;
-        cout << "2. Enviar y recibir mensajes directos, privados, aparte del chat general" << endl;
-        cout << "3. Cambiar de status" << endl;
-        cout << "4. Listar los usuarios conectados al sistema de chat" << endl;
-        cout << "5. Desplegar información de un usuario en particular" << endl;
-        cout << "6. Ayuda" << endl;
-        cout << "7. Salir" << endl;
-        
-        cout << "\nIngrese una opcion: ";
-        cin >> opcion;
-        
-        switch (opcion) {
-            case 1:
-                // Lista de instrucciones de la opción 1                
-                
-                system("pause>nul"); // Pausa
-                break;
-                
-            case 2:
-                // Lista de instrucciones de la opción 2                
-                
-                system("pause>nul"); // Pausa
-                break;
-                
-            case 3:
-                // Lista de instrucciones de la opción 3                
-                
-                system("pause>nul"); // Pausa            
-                break;
-                
-            case 4:
-                // Lista de instrucciones de la opción 4                
-                
-                system("pause>nul"); // Pausa                
-                break;
-            case 5:
-                // Lista de instrucciones de la opción 5              
-                
-                system("pause>nul"); // Pausa                
-                break;
-            case 6:
-                // Lista de instrucciones de la opción 6                
-                
-                system("pause>nul"); // Pausa                
-                break;
-            case 7:
-                // Lista de instrucciones de la opción 7                
-                
-                system("pause>nul"); // Pausa                
-                break;
+	int opcion;
+	int estado;
+	int repetir = 1;
+	int regresar = 0;
+	int result;
+	char username2chat[200];
+	char mensaje[8000];
+	char salir[] = "salir";
 
-            case 0:
-            	repetir = false;
-            	break;
-        }        
-    } while (repetir);
-	 
-    return 0;
+	while (repetir == 1) {
+
+		// Texto del menú que se verá cada vez
+		printf("\n\nMenu de Opciones");
+		printf("\n1. Chat con todos lo usuarios");
+		printf("\n2. Enviar y recibir mensajes directos, privados, aparte del chat general");
+		printf("\n3. Cambiar de status");
+		printf("\n4. Listar los usuarios conectados al sistema de chat");
+		printf("\n5. Desplegar información de un usuario en particular");
+		printf("\n6. Ayuda");
+		printf("\n7. Salir");
+
+		printf("\nIngrese una opcion: ");
+		scanf("%d", &opcion);
+
+		switch (opcion) {
+			case 1:
+				// Lista de instrucciones de la opción 1 
+				while (regresar == 0){
+					printf("Mensaje: ");
+					scanf("%s", &mensaje);
+					result = strcmp(mensaje, salir);
+					if (result == 0){
+						regresar = 1;
+					}else{
+						printf("The string you entered is : %s\n", mensaje);
+					}
+				}
+				regresar = 0;                
+				break;
+
+			case 2:
+				// Lista de instrucciones de la opción 2                
+				printf("Nombre del usuario para entablar conversacion: ");
+				scanf("%s", &username2chat);
+				printf("The string you entered is : %s\n", username2chat);
+				char username2chat[200];
+				break;
+
+			case 3:
+				// Lista de instrucciones de la opción 3   
+				printf("\nOpciones de status");
+				printf("\n1. OCUPADO");
+				printf("\n2. ACTIVO");
+				printf("\n3. INACTIVO");  
+				printf("\nIngrese una opcion: ");           
+            			scanf("%d", &estado);
+				switch(estado){
+					case 1:
+						printf("\nAhora su estado es OCUPADO");
+						break;
+					case 2:
+						printf("\nAhora su estado es ACTIVO");
+						break;
+					case 3:
+						printf("\nAhora su estado es INACTIVO");
+						break;
+					default:
+						printf("\nEsa opcion no existe. Volviendo al menu...\n");
+						sleep(1);
+				}
+			case 4:
+				// Lista de instrucciones de la opción 4                
+               
+				break;
+			case 5:
+				// Lista de instrucciones de la opción 5              
+				                
+				break;
+			case 6:
+				// Lista de instrucciones de la opción 6                
+			              
+				break;
+			case 7:
+				repetir = 0;
+				break;
+		}	
+	}
+	return 0;
 }
