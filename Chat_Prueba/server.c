b09e9c03afc838eeb5694113d5878b16a4894ce6
Carlos Alberto Ráxtum Ramos <53351491+Carlos21122000@users.noreply.github.com>
2022-05-04 18:46:29 -0600

    Update server.c

--- Chat_Prueba/server.c
+++ Chat_Prueba/server.c
@@ -267,7 +267,7 @@ int main(int argc, char **argv){
 		return EXIT_FAILURE;
 	}
 
-	char *ip = "127.0.0.1";
+	char *ip = "172.31.21.201";
 	int port = atoi(argv[1]);
 	int option = 1;
 	int listenfd = 0, connfd = 0;
